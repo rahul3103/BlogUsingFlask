c20320d0a29f984c9c529e5b163b3b35d8a78bbf
Rahul Shrivastava <rahul3103@gmail.com>
2015-06-22 12:25:29 +0530

    Commit 2
    Templates added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hi!'
+	#Creating fake user
+	user = {'nickname':'Rahul'}
+	#Creating fake posts for fake user
+	posts = [
+			{'author':{'nickname':'King'},
+			 'body':'Awesome Weather In Bengaluru!'},
+			{'author':{'nickname':'Queen'},
+			 'body':'Where is my King!'}
+			]
+	return render_template('index.html',title='Home',user=user,posts=posts)
